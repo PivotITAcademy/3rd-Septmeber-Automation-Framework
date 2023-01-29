fc33ada01d6cf06b1e19a0962bad224f6130e90d
lekshmi <lekshmi867@gmail.com>
2023-01-28 19:25:55 -0500

    added contact page

--- src/main/java/com/naveenautomation/Pages/MyAccountPage.java
+++ src/main/java/com/naveenautomation/Pages/MyAccountPage.java
@@ -7,43 +7,49 @@ import com.naveenautomation.Browsers.ProxyDriver;
 
 public class MyAccountPage extends Page {
 
-	private static final String PAGE_URL="/account";
+	private static final String PAGE_URL = "/account";
 
 	public MyAccountPage(WebDriver wd, boolean waitForPageToLoad) {
 		super(wd, waitForPageToLoad);
 	}
 
-	private static final By  myAccountText=By.xpath("//h2[text()='My Account']");
-	private static final By changePasswordLink=By.cssSelector("#column-right a:nth-of-type(3)");
-	private static final By passwordChangesSuccessBanner=By.cssSelector("div.alert-success");
-	private static final By orderHistoryLink=By.cssSelector("div.list-group a:nth-of-type(6)");
+	private static final By myAccountText = By.xpath("//h2[text()='My Account']");
+	private static final By changePasswordLink = By.cssSelector("#column-right a:nth-of-type(3)");
+	private static final By passwordChangesSuccessBanner = By.cssSelector("div.alert-success");
+	private static final By orderHistoryLink = By.cssSelector("div.list-group a:nth-of-type(6)");
+	private static final By phonelink = By.cssSelector("i.fa.fa-phone");
+
+	public ContactPage clickPhoneLink() {
+		((ProxyDriver) wd).click(phonelink);
+		return new ContactPage(wd, true);
+	}
 
 	public String getMyAccountText() {
-		return ((ProxyDriver)wd).getText(myAccountText, 10);
+		return ((ProxyDriver) wd).getText(myAccountText, 10);
 	}
 
 	public ChangePasswordPage clickChangePassword() {
-		((ProxyDriver)wd).click(changePasswordLink);
-		return new ChangePasswordPage(wd,true);
+		((ProxyDriver) wd).click(changePasswordLink);
+		return new ChangePasswordPage(wd, true);
 	}
 
 	public String getPasswordChangeSuccessMessage() {
-		return ((ProxyDriver)wd).getText(passwordChangesSuccessBanner, 10);
+		return ((ProxyDriver) wd).getText(passwordChangesSuccessBanner, 10);
 	}
-	
+
 	public OrderHistoryPage clickOrderHistoryLink() {
-		((ProxyDriver)wd).click(orderHistoryLink);
-		return new OrderHistoryPage(wd,true);
+		((ProxyDriver) wd).click(orderHistoryLink);
+		return new OrderHistoryPage(wd, true);
 	}
 
 	@Override
 	protected void isLoaded() {
 
-		if(!urlContains(wd.getCurrentUrl())) {
+		if (!urlContains(wd.getCurrentUrl())) {
 			throw new Error();
 		}
 	}
-	
+
 	@Override
 	protected String getPageUrl() {
 		return getDomain() + PAGE_URL;
