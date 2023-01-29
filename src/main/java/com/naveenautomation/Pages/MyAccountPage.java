97c908d3be5486f1870f4a2c51c47c4f000f5555
neethumanu1213 <neethumanu1231@gmail.com>
2023-01-29 18:02:18 -0500

    Added MyAccountInformationPage

--- src/main/java/com/naveenautomation/Pages/MyAccountPage.java
+++ src/main/java/com/naveenautomation/Pages/MyAccountPage.java
@@ -7,45 +7,52 @@ import com.naveenautomation.Browsers.ProxyDriver;
 
 public class MyAccountPage extends Page {
 
-	private static final String PAGE_URL="/account";
+	private static final String PAGE_URL = "/account";
 
 	public MyAccountPage(WebDriver wd, boolean waitForPageToLoad) {
 		super(wd, waitForPageToLoad);
 	}
 
-	private static final By  myAccountText=By.xpath("//h2[text()='My Account']");
-	private static final By changePasswordLink=By.cssSelector("#column-right a:nth-of-type(3)");
-	private static final By passwordChangesSuccessBanner=By.cssSelector("div.alert-success");
-	private static final By orderHistoryLink=By.cssSelector("div.list-group a:nth-of-type(6)");
+	private static final By myAccountText = By.xpath("//h2[text()='My Account']");
+	private static final By changePasswordLink = By.cssSelector("#column-right a:nth-of-type(3)");
+	private static final By SuccessBanner = By.cssSelector("div.alert-success");
+	private static final By orderHistoryLink = By.cssSelector("div.list-group a:nth-of-type(6)");
+	private static final By editAccountLink = By.xpath("//a[text()='Edit Account']");
 
 	public String getMyAccountText() {
-		return ((ProxyDriver)wd).getText(myAccountText, 10);
+		return ((ProxyDriver) wd).getText(myAccountText, 10);
 	}
 
 	public ChangePasswordPage clickChangePassword() {
-		((ProxyDriver)wd).click(changePasswordLink);
-		return new ChangePasswordPage(wd,true);
+		((ProxyDriver) wd).click(changePasswordLink);
+		return new ChangePasswordPage(wd, true);
 	}
 
-	public String getPasswordChangeSuccessMessage() {
-		return ((ProxyDriver)wd).getText(passwordChangesSuccessBanner, 10);
+	public String getSuccessMessageText() {
+		return ((ProxyDriver) wd).getText(SuccessBanner, 10);
 	}
-	
+
 	public OrderHistoryPage clickOrderHistoryLink() {
-		((ProxyDriver)wd).click(orderHistoryLink);
-		return new OrderHistoryPage(wd,true);
+		((ProxyDriver) wd).click(orderHistoryLink);
+		return new OrderHistoryPage(wd, true);
+	}
+
+	public MyAccountInformationPage clickEditAccountLink() {
+		((ProxyDriver) wd).click(editAccountLink);
+		return new MyAccountInformationPage(wd, true);
 	}
 
 	@Override
 	protected void isLoaded() {
 
-		if(!urlContains(wd.getCurrentUrl())) {
+		if (!urlContains(wd.getCurrentUrl())) {
 			throw new Error();
 		}
 	}
-	
+
 	@Override
 	protected String getPageUrl() {
 		return getDomain() + PAGE_URL;
 	}
+
 }
