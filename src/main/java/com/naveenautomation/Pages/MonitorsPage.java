d7a738e5f3442b6d0e35def661f1c73a40e91f67
HarinderKaur91 <kaur.harinder91@gmail.com>
2023-02-01 12:20:49 -0700

    updated as per the review

--- src/main/java/com/naveenautomation/Pages/MonitorsPage.java
+++ src/main/java/com/naveenautomation/Pages/MonitorsPage.java
@@ -5,31 +5,36 @@ import org.openqa.selenium.WebDriver;
 
 import com.naveenautomation.Browsers.ProxyDriver;
 
-public class MonitorsPage extends Page{
+public class MonitorsPage extends Page {
 
-	
-	private static final String PAGE_URL="/category&path=25_28";
+	String pageUrl = "/opencart/index.php?route=product/category&path=25_28";
 
-	
+	private String getPageUrlAfterVerification(String pageUrl) {
+		if (pageUrl.contains("/category&path=")) {
+			return pageUrl;
+		}
+		return null;
+	}
+
+	private final String PAGE_URL = getPageUrlAfterVerification(pageUrl);
+
 	public MonitorsPage(WebDriver wd, boolean waitForPageToLoad) {
 		super(wd, waitForPageToLoad);
 	}
-	
-	
-	public static final By firstProductAddToWishlistButton = By
-			.cssSelector("button[onclick*=\"wishlist.add('42')\"]");
-	
+
+	public static final By firstProductAddToWishlistButton = By.cssSelector("button[onclick*=\"wishlist.add('42')\"]");
+
 	public static final By wishListLink = By.cssSelector("ul.list-inline>li:nth-of-type(3) span");
-	
+
 	public void addToWishListFirstMonitor() {
 		((ProxyDriver) wd).click(firstProductAddToWishlistButton);
 	}
-	
+
 	public MyWishListPage clickWishlistLink() {
 		((ProxyDriver) wd).click(wishListLink);
 		return new MyWishListPage(wd, true);
 	}
-	
+
 	@Override
 	protected void isLoaded() {
 
@@ -37,7 +42,12 @@ public class MonitorsPage extends Page{
 			throw new Error();
 		}
 	}
-	
+
+	@Override
+	public MonitorsPage get() {
+		return (MonitorsPage) super.get();
+	}
+
 	@Override
 	protected String getPageUrl() {
 		return getDomain() + PAGE_URL;
