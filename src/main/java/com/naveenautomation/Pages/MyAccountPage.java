55455b66622c0a66fcdf4a1c717762d26b79cba3
Parul4214 <parul.parul4291@gmail.com>
2023-02-01 20:50:38 -0500

    Test case for Special Page added

--- src/main/java/com/naveenautomation/Pages/MyAccountPage.java
+++ src/main/java/com/naveenautomation/Pages/MyAccountPage.java
@@ -23,8 +23,9 @@ public class MyAccountPage extends Page {
 	private static final By editAccountLink = By.xpath("//a[text()='Edit Account']");
 	private static final By registerForAnAffiliateAccount = By.cssSelector("a[href$='affiliate/add']");
 	private static final By affiliateAccountCreationSuccessText = By.cssSelector("div.alert");
-  private static final By cameraLink = By.cssSelector("div.navbar-collapse ul>li:nth-of-type(7) a");
+	private static final By cameraLink = By.cssSelector("div.navbar-collapse ul>li:nth-of-type(7) a");
 	private static final By addToReturnLink = By.xpath("(//a[text()='Returns']) [2]");
+	private static final By clickOnSpecialsLink = By.xpath("//a[text()=\"Specials\"]");
 
 	public ProductReturnsPage clickaddToReturnLink() {
 		((ProxyDriver) wd).click(addToReturnLink);
@@ -78,6 +79,12 @@ public class MyAccountPage extends Page {
 	public CamerasPage clickCameraLink() {
 		((ProxyDriver) wd).click(cameraLink);
 		return new CamerasPage(wd, true);
+	}
+
+	public SpecialsPage clickOnSpeacialInExtras() {
+		((ProxyDriver) wd).click(clickOnSpecialsLink);
+		return new SpecialsPage(wd, true);
+
 	}
 
 	@Override
